750c74c209a2516fcabae7b367e14f856a5d615a
Anshu <priyadarshini.anshu@gmail.com>
2025-12-23 11:54:59 +0530

    first commit before dvc

---
+++ mycode.py
@@ -0,0 +1,16 @@
+# https://github.com/vikashishere/YT-MLOPS-DVC-DataVersion/blob/main/mycode.py
+import pandas as pd
+import os 
+
+data = {"name":['alice','bob','charlie'],
+"age":[20,30,40],
+"gender":['F','M','M']
+}
+df = pd.DataFrame(data)
+data_dir = "data"
+os.makedirs(data_dir,exist_ok=True)
+
+file_path = os.path.join(data_dir, 'sample_data.csv')
+df.to_csv(file_path, index=False)
+print(f"csvdata saved successfully at {data_dir}")
+
